c0102cdb177d929891c92537cc581258fdb71ae9
Babatunde Joseph <babatundejoseph85@gmail.com>
2024-06-08 18:56:59 +0100

    feat: added products from static data and product detail

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,14 @@
+import data from '@/lib/data';
+import ProductItem from '../../components/products/product-item';
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl py-2">Latest Products</h1>
+      <h2 className="text-2xl py-2">Latest Products</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.slug} product={product} />
+        ))}
+      </div>
     </>
   );
 }
